1d7ab071151a1461b23db037a3e0cec1069c3a84
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-09-26 16:58:08 +0300

    feat: add validation on RegisterDTO.cs and LoginDTO.cs

--- Contracts/Auth/RegisterDTO.cs
+++ Contracts/Auth/RegisterDTO.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Contracts.Auth;
 
 public class RegisterDTO
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required, MinLength(8)]
     public string Password { get; set; } = string.Empty;
+    [Required, MinLength(3), MaxLength(50)]
     public string Username { get; set; } = string.Empty;
 }
